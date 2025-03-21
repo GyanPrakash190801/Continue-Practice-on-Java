c6bd381513bd61ed99636c9866d00de5dcc9ed05
Yashaswi kumar <yashaswiec123@gmail.com>
2025-03-21 18:20:38 +0530

    first commit

---
+++ Day_1_a.java
@@ -0,0 +1,9 @@
+
+//Print the number 108 using arithmetic operations on the numbers 9 and 12.
+public class Day_1_a {
+
+    public static void main(String[] args) {
+        System.out.println(9*12);
+    }
+}
+
